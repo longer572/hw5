9aa638f2d1ac96ae3ac19e4bfee6d6693c6e4eb0
Ryan Long <rwlong@usc.edu>
2023-04-05 13:19:39 -0700

    Submitting hw

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,6 +20,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day,
+    int col
+);
+
+bool hasMaxShifts(Worker_T id, const DailySchedule& sched, const size_t maxShifts, int i, int j, int cnt);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,8 +47,66 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // Initialize empty schedule
+    for (int i = 0; i < (int) avail.size(); i++) {
+        sched.push_back(vector<Worker_T>());
+    }
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
 
 
 
 }
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day,
+    int col
+)
+{
+    // Base case????
+    if (day >= (int) avail.size()) return true;
+
+    // Set variables for next iteration
+    int d, c;
+    if (col == (int) dailyNeed - 1) {
+        d = day + 1;
+        c = 0;
+    } else {
+        d = day;
+        c = col + 1;
+    }
+
+    // Backtracking search
+    for (Worker_T i = 0; i < avail[day].size(); i++) {
+        // If worker is available on this day
+        if(avail[day][i] == 1 && find(sched[day].begin(), sched[day].end(), i) == sched[day].end()) {
+            // Try putting the worker i in this slot
+            sched[day].push_back(i);
+            // If worker does not have max shifts and shifts are needed to be filled
+            if (sched[day].size() <= dailyNeed && !(hasMaxShifts(i, sched, maxShifts, 0, 0, 0))) {
+                bool verify = scheduleHelper(avail, dailyNeed, maxShifts, sched, d, c);
+                if (verify) return true;
+            }
+            sched[day].pop_back();
+        }
+    }
+    return false;
+}
+
+bool hasMaxShifts(Worker_T id, const DailySchedule& sched, const size_t maxShifts, int i, int j, int cnt)
+{
+    if (sched[i][j] == id) cnt++;
+    if (cnt > (int) maxShifts) return true;
+
+    if (i == (int) sched.size() - 1 && j == (int) sched[i].size() - 1) {
+        return false;
+    } else if (j == (int) sched[i].size() - 1) {
+        if (sched[i+1].size() == 0) return false;
+        return hasMaxShifts(id, sched, maxShifts, i+1, 0, cnt);
+    } else {
+        return hasMaxShifts(id, sched, maxShifts, i, j+1, cnt);
+    } 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,14 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::string currentWord,
+    std::set<string>& words,
+    int index,
+    int numDashes);
 
 
 // Definition of primary wordle function
@@ -21,8 +29,65 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    // Number of dashes - used in helepr function
+    int n = 0;
+    for (int i=0; i < (int) in.size(); i++) {
+        if (in[i] == '-') {
+            n++;
+        }
+    }
+
+    std::set<std::string> result;
+    wordleHelper(in, floating, dict, "", result, 0, n);
+    return result;
 
 }
 
 // Define any helper functions here
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::string currentWord,
+    std::set<string>& words,
+    int index,
+    int numDashes)
+{
+    // If a word has been fully completed
+    if (index > (int) in.size() - 1) {
+        if (dict.find(currentWord) != dict.end()) {
+            words.insert(currentWord);
+        }
+        return;
+    }
+
+    // If the current letter is already fixed
+    if (in[index] != '-') {
+        currentWord += in[index]; 
+        wordleHelper(in, floating, dict, currentWord, words, index+1, numDashes);
+        return;
+
+    // If the current letter is not fixed
+    } else {
+
+        // Need to know how if we can use a non-floating letter (ex: 4 -'s, 3 floating letters left)
+        int sizeDif = numDashes - floating.size();
+        for (size_t i = 0; i < floating.size(); i++) {
+            std::string newCurrentWord = currentWord;
+            newCurrentWord += floating[i];
+            std::string newFloating = floating;
+            newFloating.erase(newFloating.begin() + i);
+            wordleHelper(in, newFloating, dict, newCurrentWord, words, index+1, numDashes-1);
+        }
+        
+        // Trying all non-floating letter combinations if necessary
+        if (sizeDif > 0) {
+            for (int i = 0; i < 26; i++) {
+                std::string newCurrentWord = currentWord;
+                newCurrentWord += ('a' + i);
+                wordleHelper(in, floating, dict, newCurrentWord, words, index+1, numDashes-1);
+            }
+        }
+        return;
+    }
+}
